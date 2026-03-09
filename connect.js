23aa4f2651df1a162b97afb2a49167ab0d8b0732
devaharshini1623 <devaharshini1810@gmail.com>
2026-03-09 18:48:02 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/NewChat');
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected");
-    }
-    catch(err){
-        console.log("MongoDB connection error:",err);
+    } catch (err) {
+        console.log("MongoDB connection error:", err);
+        process.exit(1); 
     }
 };
 
