0c335bf6131b7a8066dd78aaea45f7308cbe0514
devaharshini1623 <devaharshini1810@gmail.com>
2026-03-09 18:01:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const connectDB = require('./connect');
 
 const http = require('http');
 const { Server } = require('socket.io');
-
+const cors = require('cors');
 const Message = require('./models/Message'); 
 
 const app = express();
 
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server,{
+  cors:{
+    origin:"*"
+  }
+});
 
 app.set("view engine","ejs");
 
@@ -73,3 +77,4 @@ const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
